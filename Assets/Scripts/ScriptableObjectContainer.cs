3a81d0f4329ce18f4a797a52180c579fd181c860
Dan Miller <dsmiller95@gmail.com>
2019-12-27 13:01:43 -0600

    cleaned up scene, added new script to start on the scriptable object layout

---
+++ Assets/Scripts/ScriptableObjectContainer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UIElements;
+
+public class ScriptableObjectContainer : VisualElement
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
