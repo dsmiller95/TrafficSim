ffa97b8e9f011fb97b25dccdb2df64f2c7cb02b0
Dan Miller <dsmiller95@gmail.com>
2019-12-27 17:39:19 -0600

    created node-based drag-drop and chaining component

--- Assets/Scripts/DragDropLockable.cs
+++ Assets/Scripts/DragDropLockable.cs
@@ -1,43 +1,144 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class DragDropLockable : EventTrigger
 {
+    public static DragDropLockable CurrentDragging;
+
+    private Image baseImage;
     private bool dragging;
-    private Vector2 dragStartDiff;
+    private ISet<DragDropLockable> dragLinked = new HashSet<DragDropLockable>();
+
+    private DragDropLockable parent;
+    private DragDropLockable child;
+
+    private RectTransform rectTransform
+    {
+        get => this.GetComponent<RectTransform>();
+    }
 
     // Use this for initialization
     void Start()
     {
-
+        this.baseImage = this.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dragging)
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        if (this.dragging)
         {
-            var newPos = this.GetMousePosOnCanvas() + this.dragStartDiff;
-            Debug.Log($"dragging to ${newPos}");
-            transform.position = newPos;
+            return;
+        }
+
+        this.baseImage.color = Color.green;
+        if(CurrentDragging != null)
+        {
+            CurrentDragging.dragLinked.Add(this);
         }
     }
 
-    public override void OnPointerDown(PointerEventData eventData)
+    public override void OnPointerExit(PointerEventData eventData)
     {
-        this.dragStartDiff = new Vector2(this.transform.position.x, this.transform.position.y) - this.GetMousePosOnCanvas();
-        dragging = true;
+        if (this.dragging)
+        {
+            return;
+        }
+        this.baseImage.color = Color.white;
+        if (CurrentDragging != null)
+        {
+            CurrentDragging.dragLinked.Remove(this);
+        }
     }
 
-    public override void OnPointerUp(PointerEventData eventData)
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        dragging = true;
+        this.transform.SetAsFirstSibling();
+        this.baseImage.color = Color.blue;
+        CurrentDragging = this;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if(this.parent != null)
+        {
+            this.parent.child = null;
+            this.parent = null;
+        }
+        this.transform.position += new Vector3(eventData.delta.x, eventData.delta.y);
+        this.child?.UpdatePositionRelativeToParent();
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
     {
         dragging = false;
+        this.baseImage.color = Color.white;
+        CurrentDragging = null;
+        this.AttachToParent(this.dragLinked.FirstOrDefault());
+        this.dragLinked.Clear();
+    }
+
+    /// <summary>
+    /// Return a transform to apply to a child relative to this object when linking
+    /// </summary>
+    /// <returns></returns>
+    private Vector3 GetChildTransform()
+    {
+        return Vector3.down * this.rectTransform.sizeDelta.y;
+    }
+
+    private void UpdatePositionRelativeToParent()
+    {
+        this.transform.position = parent.transform.position + parent.GetChildTransform();
+        this.child?.UpdatePositionRelativeToParent();
+    }
+
+    private void AttachToParent(DragDropLockable parent)
+    {
+        if(parent == null)
+        {
+            return;
+        }
+        this.parent = parent;
+        if (this.parent.child)
+        {
+            this.CascadeFloatingChild(this.parent.child);
+        }
+        this.parent.child = this;
+        this.UpdatePositionRelativeToParent();
+    }
+
+    /// <summary>
+    /// After a drag completes on top of a Lockable with children, insert the new block of Lockables directly below the target
+    ///     And cascade all previous children to the bottom of the chain
+    /// </summary>
+    /// <param name="child">The child to be dropped to the bottom of the current list</param>
+    private void CascadeFloatingChild(DragDropLockable child)
+    {
+        if (this.child)
+        {
+            this.child.CascadeFloatingChild(child);
+        }
+        else
+        {
+            this.child = child;
+            this.child.parent = this;
+            this.child.UpdatePositionRelativeToParent();
+        }
     }
 
     private Vector2 GetMousePosOnCanvas()
     {
-        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var pos = Input.mousePosition;
         return new Vector2(pos.x, pos.y);
     }
 }
